1a82ba588a49fb0831a3c4dee0d508b63559b062
1sagarthakur1 <sagar5710@gmail.com>
2023-10-16 23:26:11 +0530

    add Indivisual Project

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import {Routes, Route} from 'react-router-dom';
 import '../Style/style.css'
 
@@ -20,14 +20,17 @@ export default function Navbar() {
       x.className = "drop_box_c";
     }
   }
+  const [bool,setBool] = useState(true);
 
   const navigate = useNavigate();
 
   const handleClick1 = () =>{
     navigate('/')
+    setBool(true);
   }
   const handleClick2 = () =>{
     navigate('/indivisualP')
+    setBool(false);
   }
 
   const handleButtonClick1 = () => {
@@ -39,6 +42,35 @@ export default function Navbar() {
     handleClick2();
 };
 
+
+if(bool===false){
+  const aboutme = document.getElementById("aboutme");
+  aboutme.style.display = 'none';
+  const skills = document.getElementById("skills");
+  skills.style.display = 'none';
+  const service = document.getElementById("service");
+  service.style.display = 'none';
+  const contect = document.getElementById("contect");
+  contect.style.display = 'none';
+  const resume = document.getElementById("resume");
+  resume.style.display = 'none';
+  const indiProject = document.getElementById("indiProject");
+  indiProject.style.display = 'none';
+}
+else if(bool===true){
+  const aboutme = document.getElementById("aboutme");
+  aboutme.style.display = 'block';
+  const skills = document.getElementById("skills");
+  skills.style.display = 'block';
+  const service = document.getElementById("service");
+  service.style.display = 'block';
+  const contect = document.getElementById("contect");
+  contect.style.display = 'block';
+  const resume = document.getElementById("resume");
+  resume.style.display = 'block';
+  const indiProject = document.getElementById("indiProject");
+  indiProject.style.display = 'block';
+}
   
   return (
     <>
@@ -48,13 +80,13 @@ export default function Navbar() {
               <img className='name_logo' src={name_logo} alt="" />
             </div>
             <div className="all_links">
-              <li onClick={handleClick1}><a href="#section1">Home</a></li>
-              <li><a href="#section2">About me</a></li>
-              <li><a href="#section3">Skills</a></li>
-              <li><a href="#section4">Service</a></li>
-              <li><a href="#section5">Contect</a></li>
-              <li onClick={openResume}><a href="#section5">Resume</a></li>
-              <li onClick={handleClick2}>Indivisual Projects</li>
+              <li id='home' onClick={handleClick1}><a href="#section1">Home</a></li>
+              <li id='aboutme'><a href="#section2">About me</a></li>
+              <li id='skills'><a href="#section3">Skills</a></li>
+              <li id="service"><a href="#section4">Service</a></li>
+              <li id='contect'><a href="#section5">Contect</a></li>
+              <li id='resume' onClick={openResume}><a href="#section5">Resume</a></li>
+              <li id='indiProject' onClick={handleClick2}>Indivisual Projects</li>
             </div>
             <div onClick={myFunction} className="toggle">
               <div></div>
@@ -63,13 +95,13 @@ export default function Navbar() {
             </div>
         </div>
         <div className="drop_box_c" id="myTopnav">
-              <li onClick={handleButtonClick1}><a href="#section1">Home</a></li>
-              <li onClick={myFunction}><a href="#section2">About me</a></li>
-              <li onClick={myFunction}><a href="#section3">Skills</a></li>
-              <li onClick={myFunction}><a href="#section4">Service</a></li>
-              <li onClick={myFunction}><a href="#section5">Contect</a></li>
-              <li onClick={myFunction}><a href="#section5">Resume</a></li>
-              <li onClick={handleButtonClick2}><a href="#section5">Indivisual Projcets</a></li>
+              <li id='home' onClick={handleButtonClick1}><a href="#section1">Home</a></li>
+              <li id='aboutme' onClick={myFunction}><a href="#section2">About me</a></li>
+              <li id='skills'onClick={myFunction}><a href="#section3">Skills</a></li>
+              <li id="service" onClick={myFunction}><a href="#section4">Service</a></li>
+              <li id='contect' onClick={myFunction}><a href="#section5">Contect</a></li>
+              <li id='resume' onClick={myFunction}><a href="#section5">Resume</a></li>
+              <li id='indiProject' onClick={handleButtonClick2}><a href="#section5">Indivisual Projcets</a></li>
         </div>
       </nav>
     </>
